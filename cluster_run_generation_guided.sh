5785f338fc2a666287176fc56fe58fc4446027d1
Jan Claar <mail@jan-claar.de>
2025-12-11 13:39:01 +0100

    correct script

--- cluster_run_generation_guided.sh
+++ cluster_run_generation_guided.sh
@@ -12,5 +12,5 @@ conda activate digress
 # Constants/Arguments
 runname=$1
 # Run training
-python guidance/main_guidance.py +experiment=guidance_nr_species.yaml dataset=switches
+python guidance/main_guidance.py +experiment=guidance_nr_species_cluster.yaml dataset=switches
 
